72a6159c2be65cffb15be13da66b377da5023137
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-03-11 23:42:22 +0700

    refactor BossBabyRevenge

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ func BossBabyRevenge(shot string) string {
 	bossRevenge := 0
 	shot = strings.ToUpper(shot)
 
-	if initFight := isBossBabyInitiateFight(shot); initFight {
+	if initiateFight := isBossBabyInitiateFight(shot); initiateFight {
 		return "Bad boy"
 	}
 
 	for _, shot := range shot {
 		if shot == 'S' {
-			bossRevenge--
-		} else if shot == 'R' && bossRevenge < 0 {
 			bossRevenge++
+		} else if shot == 'R' && bossRevenge > 0 {
+			bossRevenge--
 		}
 	}
 
-	if bossRevenge < 0 {
+	if bossRevenge > 0 {
 		return "Bad boy"
 	}
 	return "Good boy"
