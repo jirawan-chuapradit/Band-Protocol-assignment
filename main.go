51a152a04ca902ae43f822c11fd923532695ccb2
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-03-11 23:13:13 +0700

    add BossBabyRevenge

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func BossBabyRevenge(text string) string {
-	return "Good boy"
+import "strings"
+
+// S shots taken by Boss Baby,
+// R shots taken by kids
+// and then compares them to determine whether Boss Baby is a "Good boy" or a "Bad boy"
+func BossBabyRevenge(shot string) string {
+	kidShot := 0
+	shot = strings.ToUpper(shot)
+
+	// boss_baby_initiate_fight
+	if strings.HasPrefix(shot, "R") {
+		return "Bad boy"
+	}
+
+	for _, shot := range shot {
+		if shot == 'S' {
+			kidShot++
+		} else if shot == 'R' && kidShot > 0 {
+			kidShot--
+		}
+	}
+
+	if kidShot <= 0 {
+		return "Good boy"
+	} else {
+		return "Bad boy"
+	}
 }
